9208028d71bd7756367f7b9ebf260b493e11f9db
Art <artshkololli@gmail.com>
2023-06-18 00:18:40 +0200

    Small fix

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -42,7 +42,7 @@ app.get("/api/foods/:foodId", (req, res) => {
 
 app.post("api/users/login", (req,res)=>{
   const {email,password}=req.body;
-  const user=sample_users.find(user=>user.email ===email && user.password===password);
+  const user=sample_users.find(user=>user.email === email && user.password===password);
 
   if(user){
     res.send();
