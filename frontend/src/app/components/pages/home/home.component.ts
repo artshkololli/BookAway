0ab11beb89c18178e3eda025469c0aac1872956e
Art <artshkololli@gmail.com>
2023-06-22 23:58:27 +0200

    Fixed tags

--- frontend/src/app/components/pages/home/home.component.ts
+++ frontend/src/app/components/pages/home/home.component.ts
@@ -9,25 +9,26 @@ import { Food } from 'src/app/shared/models/Food';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit{
+export class HomeComponent implements OnInit {
 
-  foods:Food[]=[];
-  constructor(private foodService:FoodService,activatedRoute:ActivatedRoute){
-    let foodsObservable:Observable<Food[]>
-    activatedRoute.params.subscribe((params)=>{
-      if(params.searchTerm){
-        foodsObservable=this.foodService.getAllFoodsBySearch(params.searchTerm);
-      }else if(params.tag){
-        foodsObservable=this.foodService.getAllFoodsByTag(params.tag);
-      }else{
-        foodsObservable=foodService.getAll();
-        foodsObservable.subscribe(serverFoods =>{
-          this.foods=serverFoods
+  foods: Food[] = [];
+  constructor(private foodService: FoodService, activatedRoute: ActivatedRoute) {
+    let foodsObservalbe:Observable<Food[]>;
+    activatedRoute.params.subscribe((params) => {
+      if (params.searchTerm)
+        foodsObservalbe = this.foodService.getAllFoodsBySearchTerm(params.searchTerm);
+      else if (params.tag)
+        foodsObservalbe = this.foodService.getAllFoodsByTag(params.tag);
+      else
+        foodsObservalbe = foodService.getAll();
+
+        foodsObservalbe.subscribe((serverFoods) => {
+          this.foods = serverFoods;
         })
-      }
-    });
+    })
   }
 
   ngOnInit(): void {
   }
+
 }
--- frontend/src/app/services/food.service.ts
+++ frontend/src/app/services/food.service.ts
@@ -1,10 +1,10 @@
-import { Injectable } from '@angular/core';
-import { Food } from '../shared/models/Food';
-import { sample_foods, sample_tags } from 'src/data';
-import { Tag } from '../shared/models/tag';
 import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { FOODS_BY_SEARCH_URL, FOODS_BY_TAG_URL,FOOD_BY_ID_URL, FOODS_TAGS_URL, FOODS_URL } from '../shared/constants/urls';
+import { sample_foods, sample_tags } from 'src/data';
+import { FOODS_BY_SEARCH_URL, FOODS_BY_TAG_URL, FOODS_TAGS_URL, FOODS_URL, FOOD_BY_ID_URL } from '../shared/constants/urls';
+import { Food } from '../shared/models/Food';
+import { Tag } from '../shared/models/Tag';
 
 @Injectable({
   providedIn: 'root'
@@ -13,24 +13,26 @@ export class FoodService {
 
   constructor(private http:HttpClient) { }
 
-  getAll():Observable<Food[]>{
+  getAll(): Observable<Food[]> {
     return this.http.get<Food[]>(FOODS_URL);
   }
 
-  getAllFoodsBySearch(searchTerm:string){
+  getAllFoodsBySearchTerm(searchTerm: string) {
     return this.http.get<Food[]>(FOODS_BY_SEARCH_URL + searchTerm);
   }
 
-  getAllTags():Observable<Tag[]>{
+  getAllTags(): Observable<Tag[]> {
     return this.http.get<Tag[]>(FOODS_TAGS_URL);
   }
 
-  getAllFoodsByTag(tag:string):Observable<Food[]>{
-    return tag==="All"? this.getAll() : this.http.get<Food[]>(FOODS_BY_TAG_URL+tag);
+  getAllFoodsByTag(tag: string): Observable<Food[]> {
+    return tag === "All" ?
+      this.getAll() :
+      this.http.get<Food[]>(FOODS_BY_TAG_URL + tag);
   }
 
   getFoodById(foodId:string):Observable<Food>{
-    return this.http.get<Food>(FOOD_BY_ID_URL+foodId);
+    return this.http.get<Food>(FOOD_BY_ID_URL + foodId);
   }
 
 }
