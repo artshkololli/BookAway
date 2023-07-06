3e72ed638e60154a400643753f79758c1b43d865
Art <artshkololli@gmail.com>
2023-07-06 22:43:17 +0200

    Format fix

--- frontend/src/app/components/pages/order-page/order-page.component.ts
+++ frontend/src/app/components/pages/order-page/order-page.component.ts
@@ -11,17 +11,17 @@ import { Order } from 'src/app/shared/models/Order';
 export class OrderPageComponent implements OnInit {
 
   order:Order = new Order();
+  orders?:Order[];
   constructor(orderService: OrderService, router: Router) {
-      orderService.getNewOrderForCurrentUser().subscribe({
-        next: (order) => {
-          this.order = order;
-        },
-        error:() => {
-          router.navigateByUrl('/checkout');
-        }
-      })
-
-   }
+    orderService.getNewOrderForCurrentUser().subscribe({
+      next: (order) => {
+        this.order = order;
+      },
+      error:() => {
+        router.navigateByUrl('/checkout');
+      }
+    })
+  }
 
   ngOnInit(): void {
   }
