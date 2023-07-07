085fede85edfdc590fc4be30dfbc5171e97cea3f
Art <artshkololli@gmail.com>
2023-07-07 22:18:55 +0200

    Fixed 'All' tag loading loop

--- frontend/src/app/services/book.service.ts
+++ frontend/src/app/services/book.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, retry } from 'rxjs';
 import { BOOKS_BY_SEARCH_URL, BOOKS_BY_TAG_URL, BOOKS_TAGS_URL, BOOKS_URL, BOOK_BY_ID_URL } from '../shared/constants/urls';
 import { Book } from '../shared/models/Book';
 import { Tag } from '../shared/models/Tag';
@@ -25,9 +25,12 @@ export class BookService {
   }
 
   getAllBooksByTag(tag: string): Observable<Book[]> {
-    return tag === "All" ?
-      this.getAll() :
-      this.http.get<Book[]>(BOOKS_BY_TAG_URL + tag);
+    if(tag==="All"){
+      location.replace('/');
+      return this.getAll();
+    }else{
+      return this.http.get<Book[]>(BOOKS_BY_TAG_URL + tag);
+    }
   }
 
   getBookById(bookId:string):Observable<Book>{
