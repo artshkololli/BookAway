fca97d56caa7865c939a2bd07058ddfa9cb0ffca
Art <artshkololli@gmail.com>
2023-07-15 21:39:58 +0200

    Logout redirect

--- frontend/src/app/services/user.service.ts
+++ frontend/src/app/services/user.service.ts
@@ -64,7 +64,7 @@ export class UserService {
   logout(){
     this.userSubject.next(new User());
     localStorage.removeItem(USER_KEY);
-    window.location.reload();
+    window.location.replace('/');
   }
 
   private setUserToLocalStorage(user:User){
