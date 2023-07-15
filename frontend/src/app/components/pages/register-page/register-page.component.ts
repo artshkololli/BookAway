a1a0c0503ea2e193af26e0b278814b052b6b8f20
Art <artshkololli@gmail.com>
2023-07-15 21:20:09 +0200

    Form Validation fix

--- frontend/src/app/components/pages/register-page/register-page.component.ts
+++ frontend/src/app/components/pages/register-page/register-page.component.ts
@@ -26,9 +26,9 @@ export class RegisterPageComponent implements OnInit{
 
   ngOnInit(): void {
     this.registerForm=this.formBuilder.group({
-      name:['',[Validators.required,Validators.minLength(5)]],
+      name:['',[Validators.required,Validators.minLength(3)]],
       email:['',[Validators.required,Validators.email]],
-      password:['',[Validators.required,Validators.minLength(5)]],
+      password:['',[Validators.required,Validators.minLength(6)]],
       confirmPassword:['',Validators.required],
       address:['',[Validators.required,Validators.minLength(10)]],
     },{
